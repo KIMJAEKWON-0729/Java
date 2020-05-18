d63927139712cfeea40bff85999a7c51f4ac5e04
KIMJAEKWON-0729 <53963225+KIMJAEKWON-0729@users.noreply.github.com>
2020-05-18 15:16:11 +0900

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.print("Hello Java\n");
+		System.out.print("Hello Oracle\n");
+		System.out.print("Hello Spring\n");
+	}
+}
+
+// 주석은 슬래쉬 두개 
+// println 은 줄바꿈 
+
+//"Hello Java/n 하면 찍고 줄바꿈이 가능 "
+// 클래스가 만들어져 있고 클래스 안에 name 이 들어 있고 시작은 항상 메인을 통해서 시작 args 라는 argument가 없으면 
+// 메인을 통해서 찍고 찍는다
