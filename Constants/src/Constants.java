e799cd4883b48b811ab468bca9f502a5c8a65704
KIMJAEKWON-0729 <53963225+KIMJAEKWON-0729@users.noreply.github.com>
2020-06-22 17:16:59 +0900

    Add files via upload

---
+++ Constants/src/Constants.java
@@ -0,0 +1,12 @@
+
+public class Constants {
+	public static void main(String[] args) {
+		final double MAX_SIZE = 100.1;
+		final char CONST_CHAR = '상';
+		final int CONST_ASSIGNED;
+		CONST_ASSIGNED = 12;
+		System.out.println("상수1 : "+MAX_SIZE);
+		System.out.println("상수2 : " + CONST_CHAR);
+		System.out.println("상수3: " +CONST_ASSIGNED  );
+	}
+}
---
+++ Operators/src/ArithOp.java
@@ -0,0 +1,20 @@
+
+public class ArithOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 7;
+		int num2 = 3;
+		
+		System.out.println("num1 + num2 =" + (num1+num2));
+
+		System.out.println("num1 - num2 =" + (num1-num2));
+
+		System.out.println("num1 * num2 =" + (num1*num2));
+
+		System.out.println("num1 / num2 =" + (num1/num2));
+
+		System.out.println("num1 % num2 =" + (num1%num2));
+	}
+
+}
